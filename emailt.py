ea663de751d63abab9c991dbf607323be11eda3c
Voxusss <antoine4013@gmail.com>
2020-05-27 18:37:10 +0200

    V1.0.0 / Creating Captcha Harvester rn

---
+++ emailt.py
@@ -0,0 +1,324 @@
+import random
+import csv
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import string
+import os
+import main
+import random
+import threading
+import imaplib
+import email
+import urllib.request
+from email.header import decode_header
+import webbrowser
+import os
+from selenium.webdriver.common.proxy import Proxy, ProxyType
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+def alias(driver):
+    outset = 0
+    for i in range(10):
+        time.sleep(10)
+        driver.find_element_by_xpath("//a[@id='idAddAliasLink']").click()
+        time.sleep(15)
+        with open('firstnames.txt') as fn:
+            firsts = fn.readlines()
+        l=[]
+        for i in range(0,len(firsts)-1):
+            x=firsts[i]
+            z=len(x)
+            a=x[:z-1]
+            l.append(a)
+        l.append(firsts[i+1])
+        FirstName=random.choice(l)
+        with open('lastnames.txt') as ln:
+            lasts = ln.readlines()
+        l=[]
+        for i in range(0,len(lasts)-1):
+            x=lasts[i]
+            z=len(x)
+            a=x[:z-1]
+            l.append(a)
+        l.append(lasts[i+1])
+        LastName=random.choice(l)
+        with open('password.txt') as ps2:
+            ps2 = ps2.readline()
+            ps2=ps2.split("['")[0]
+        username = FirstName + LastName
+        time.sleep(4)
+        if(driver.find_elements_by_xpath("//option[@value='outlook.com']")):
+            driver.find_element_by_xpath("//option[@value='outlook.com']").click()
+        time.sleep(4)
+        driver.find_element_by_xpath("//input[@id='AssociatedIdLive']").send_keys(username)
+        time.sleep(4)
+        driver.find_element_by_xpath("//input[@id='SubmitYes']").click()
+        time.sleep(4)
+        if(driver.find_elements_by_xpath("//div[@id='iAddErrorLive']")):
+            time.sleep(4)
+            driver.find_element_by_xpath("//input[@id='AssociatedIdLive']").clear()
+            with open('firstnames.txt') as fn:
+                firsts = fn.readlines()
+            l=[]
+            for i in range(0,len(firsts)-1):
+                x=firsts[i]
+                z=len(x)
+                a=x[:z-1]
+                l.append(a)
+            l.append(firsts[i+1])
+            FirstName=random.choice(l)
+            with open('lastnames.txt') as ln:
+                lasts = ln.readlines()
+            l=[]
+            for i in range(0,len(lasts)-1):
+                x=lasts[i]
+                z=len(x)
+                a=x[:z-1]
+                l.append(a)
+            l.append(lasts[i+1])
+            LastName=random.choice(l)
+            username = FirstName + LastName
+            if(driver.find_elements_by_xpath("//option[@value='outlook.com']")):
+                driver.find_element_by_xpath("//option[@value='outlook.com']").click()
+            time.sleep(4)
+            driver.find_element_by_xpath("//input[@id='AssociatedIdLive']").send_keys(username)
+            time.sleep(4)
+            driver.find_element_by_xpath("//input[@id='SubmitYes']").click()
+            time.sleep(4)
+            if(driver.find_elements_by_xpath("//div[@id='iAddErrorLive']")):
+                time.sleep(4)
+                driver.find_element_by_xpath("//input[@id='AssociatedIdLive']").clear()
+                with open('firstnames.txt') as fn:
+                    firsts = fn.readlines()
+                l=[]
+                for i in range(0,len(firsts)-1):
+                    x=firsts[i]
+                    z=len(x)
+                    a=x[:z-1]
+                    l.append(a)
+                l.append(firsts[i+1])
+                FirstName=random.choice(l)
+                with open('lastnames.txt') as ln:
+                    lasts = ln.readlines()
+                l=[]
+                for i in range(0,len(lasts)-1):
+                    x=lasts[i]
+                    z=len(x)
+                    a=x[:z-1]
+                    l.append(a)
+                l.append(lasts[i+1])
+                LastName=random.choice(l)
+                username = FirstName + LastName
+                if(driver.find_elements_by_xpath("//option[@value='outlook.com']")):
+                    driver.find_element_by_xpath("//option[@value='outlook.com']").click()
+                time.sleep(4)
+                driver.find_element_by_xpath("//input[@id='AssociatedIdLive']").send_keys(username)
+                time.sleep(4)
+                driver.find_element_by_xpath("//input[@id='SubmitYes']").click()
+                time.sleep(4)
+        emailytu = username + "@outlook.com:"+ps2
+        with open('emails+aliases.txt', 'a+') as file:
+            file.write("\n" + emailytu)
+        with open("emails+aliases.txt","r") as f:
+                lines=f.readlines()
+        with open("emails+aliases.txt","w") as f:
+            [f.write(line) for line in lines if line.strip() ]
+        time.sleep(4)
+    driver.quit()
+def imap(driver):
+    time.sleep(4)
+    with open('password.txt') as ps2:
+        ps2 = ps2.readline()
+        ps2=ps2.split("['")[0]
+    with open("recupemail.txt", "r") as recem:
+        recems = recem.readlines()
+        r=[]
+        for i in range(0,len(recems)-1):
+            x=recems[i]
+            z=len(x)
+            a=x[:z-1]
+            r.append(a)
+        r.append(recems[i+1])
+        recemchose=random.choice(r)
+    with open("recupemail.txt", "r+") as f:
+        d = f.readlines()
+        f.seek(0)
+        for i in d:
+            if recemchose not in i:
+                f.write(i)
+        f.truncate()
+    email_user=recemchose.split(":")[0]
+    email_pass=recemchose.split(":")[1]
+    driver.find_element_by_xpath("//input[@id='EmailAddress']").send_keys(email_user)
+    time.sleep(4)
+    driver.find_element_by_xpath("//input[@id='iNext']").click()
+    time.sleep(20)
+    mail = imaplib.IMAP4_SSL('outlook.office365.com')
+    mail.login(email_user, email_pass)
+    mail.select()
+    type, data = mail.search(None, 'ALL')
+    mail_ids = data[0]
+    id_list = mail_ids.split()
+    for num in data[0].split():
+        typ, data = mail.fetch(num, '(RFC822)' )
+        raw_email = data[0][1]
+        raw_email_string = raw_email.decode('utf-8')
+        email_message = email.message_from_string(raw_email_string)
+    subject = str(email_message).split("Subject: ", 1)[1].split("\nTo:", 1)[0]
+    codesec = str(email_message).split("Security code: ", 1)[1].split("If", 1)[0]
+    codesec = str(codesec)
+    driver.find_element_by_xpath("//input[@id='iOttText']").send_keys(codesec)
+    time.sleep(4)
+    driver.find_element_by_xpath("//img[@class='tile-img']").click()
+    time.sleep(4)
+    driver.find_element_by_xpath("//input[@name='ProofConfirmation']").send_keys(email_user)
+    driver.find_element_by_xpath("//input[@id='idSubmit_SAOTCS_SendCode']").click()
+    time.sleep(20)
+    mail = imaplib.IMAP4_SSL('outlook.office365.com')
+    mail.login(email_user, email_pass)
+    mail.select()
+    type, data = mail.search(None, 'ALL')
+    mail_ids = data[0]
+    id_list = mail_ids.split()
+    for num in data[0].split():
+        typ, data = mail.fetch(num, '(RFC822)' )
+        raw_email = data[0][1]
+        raw_email_string = raw_email.decode('utf-8')
+        email_message = email.message_from_string(raw_email_string)
+    subject = str(email_message).split("Subject: ", 1)[1].split("\nTo:", 1)[0]
+    codesec = str(email_message).split("Security code: ", 1)[1].split("If", 1)[0]
+    codesec = str(codesec)
+    driver.find_element_by_xpath("//input[@name='otc']").send_keys(codesec)
+    time.sleep(12)
+    driver.find_element_by_xpath("//a[@id='iCancel']").click()
+    time.sleep(10)
+    alias(driver)
+def emailio():
+    with open('firstnames.txt') as fn:
+        firsts = fn.readlines()
+    l=[]
+    for i in range(0,len(firsts)-1):
+        x=firsts[i]
+        z=len(x)
+        a=x[:z-1]
+        l.append(a)
+    l.append(firsts[i+1])
+    FirstName=random.choice(l)
+    with open('lastnames.txt') as ln:
+        lasts = ln.readlines()
+    l=[]
+    for i in range(0,len(lasts)-1):
+        x=lasts[i]
+        z=len(x)
+        a=x[:z-1]
+        l.append(a)
+    l.append(lasts[i+1])
+    LastName=random.choice(l)
+    username = FirstName + LastName
+    with open('password.txt') as ps2:
+        ps2 = ps2.readline()
+        ps2=ps2.split("['")[0]
+    month = random.randint(1,12)
+    day = random.randint(13,30)
+    year = random.randint(1960,1998)
+    unleash = 0
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_experimental_option("detach", True)
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--window-size=1,725")
+    chrome_options.add_argument("--disable-setuid-sandbox")
+    if (os.stat("proxies.txt").st_size != 0):
+        prox = Proxy()
+        with open('proxies.txt') as pr:
+            proxies = pr.readlines()
+        p=[]
+        for i in range(0,len(proxies)-1):
+            x=proxies[i]
+            z=len(x)
+            a=x[:z-1]
+            p.append(a)
+        p.append(proxies[i+1])
+        ip=random.choice(p)
+        prox.proxy_type = ProxyType.MANUAL
+        prox.http_proxy = ip
+        prox.ssl_proxy = ip
+        capabilities = webdriver.DesiredCapabilities.CHROME
+        prox.add_to_capabilities(capabilities)
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_experimental_option("detach", True)
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument('--window-size=1920,1080')
+    chrome_options.add_argument("--disable-setuid-sandbox")
+    if (os.stat("proxies.txt").st_size != 0):
+        driver = webdriver.Chrome(options=chrome_options, desired_capabilities=capabilities)
+    else:
+        driver = webdriver.Chrome(options=chrome_options)
+    driver.get("https://signup.live.com/signup")
+    time.sleep(4)
+    driver.find_element_by_xpath("//a[@id='liveSwitch']").click()
+    time.sleep(4)
+    driver.find_element_by_xpath("//a[@id='easiSwitch']").click()
+    time.sleep(4)
+    driver.find_element_by_xpath("//a[@id='liveSwitch']").click()
+    time.sleep(4)
+    driver.find_element_by_xpath("//select[@id='LiveDomainBoxList']").click()
+    time.sleep(2)
+    driver.find_element_by_xpath("//option[@value='outlook.com']").click()
+    time.sleep(2)
+    driver.find_element_by_xpath("//input[@id='MemberName']").send_keys(username)
+    time.sleep(4)
+    driver.find_element_by_xpath("//input[@id='iSignupAction']").click()
+    time.sleep(8)
+    driver.find_element_by_xpath("//input[@id='PasswordInput']").send_keys(ps2)
+    time.sleep(2)
+    driver.find_element_by_xpath("//input[@id='iSignupAction']").click()
+    time.sleep(2)
+    driver.find_element_by_xpath("//input[@id='FirstName']").send_keys(FirstName)
+    driver.find_element_by_xpath("//input[@id='LastName']").send_keys(LastName)
+    driver.find_element_by_xpath("//input[@id='iSignupAction']").click()
+    time.sleep(4)
+    driver.find_elements_by_xpath("//option[@value='"+str(month)+"']")[0].click()
+    time.sleep(2)
+    driver.find_element_by_xpath("//option[@value='"+str(day)+"']").click()
+    time.sleep(2)
+    driver.find_element_by_xpath("//option[@value='"+str(year)+"']").click()
+    time.sleep(2)
+    driver.find_element_by_xpath("//input[@id='iSignupAction']").click()
+
+    time.sleep(4)
+    img = driver.find_element_by_xpath("//img[contains(@aria-label, 'Visual')][contains(@aria-label, 'Challenge')]")
+    src = img.get_attribute('src')
+    with open("nb.txt", 'r+') as capn:
+        nbvalue = int(capn.read())
+        nbvalue = nbvalue +1
+        print(nbvalue)
+        capn.seek(0)
+        capn.truncate()
+        capn.write(str(nbvalue))
+    with open("captchaadress.txt", 'a+') as cap:
+        cap.write(src +" / "+ str(nbvalue)+"\n")
+    element = WebDriverWait(driver, 100000).until(EC.presence_of_element_located((By.XPATH, "//a[@id='help-links-msa-info']")))
+    emailwr= username+ "@outlook.com"
+    with open('emails.txt', 'a+') as file:
+        file.write("\n" + emailwr + ":"+ ps2)
+    with open("emails.txt","r") as f:
+            lines=f.readlines()
+    with open("emails.txt","w") as f:
+        [f.write(line) for line in lines if line.strip() ]
+    with open('emails+aliases.txt', 'a+') as file:
+        file.write("\n" + emailwr + ":"+ ps2)
+    with open("emails+aliases.txt","r") as f:
+            lines=f.readlines()
+    with open("emails+aliases.txt","w") as f:
+        [f.write(line) for line in lines if line.strip() ]
+    driver.get("https://outlook.live.com/owa/")
+    element = WebDriverWait(driver, 100000).until(EC.presence_of_element_located((By.XPATH, "//span[@data-automationid='splitbuttonprimary']")))
+    driver.get("https://account.microsoft.com/profile/")
+    element = WebDriverWait(driver, 100000).until(EC.presence_of_element_located((By.XPATH, "//a[@cms-link='$ctrl.links.personalManageEmail']")))
+    element.click()
+    time.sleep(10)
+    driver.find_element_by_xpath("//option[@value='Email']").click()
+    imap(driver)
+emailio()
---
+++ main.py
@@ -0,0 +1,187 @@
+import random
+import csv
+import eel
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import string
+import subprocess
+import random
+import imaplib
+import email
+from email.header import decode_header
+import webbrowser
+import os
+import traceback
+import threading
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+eel.init("web")
+captchaIndex = 0
+with open("nb.txt", 'r+') as capn:
+    capn.write("0")
+with open("captchavalue.txt", 'r+') as capv:
+    capv.seek(0)
+    capv.truncate()
+@eel.expose
+def writecaptchavalue(captchaValue, captchaValueIndex):
+    with open("captchavalue.txt", "a+") as capvalue:
+        print(captchaValue)
+        if (captchaValueIndex != 0):
+            capvalue.write(str(captchaValue)+"/"+str(captchaValueIndex)+"\n")
+def every(delay, task):
+  next_time = time.time() + delay
+  while True:
+    time.sleep(max(0, next_time - time.time()))
+    try:
+      task()
+    except Exception:
+      traceback.print_exc()
+      # in production code you might want to have this instead of course:
+      # logger.exception("Problem while executing repetitive task.")
+    # skip tasks if we are behind schedule:
+    next_time += (time.time() - next_time) // delay * delay + delay
+@eel.expose
+def updateCapIndex():
+    global captchaIndex
+    captchaIndex = captchaIndex+1
+def wala():
+    nbcaptchas = 0
+    urlimg = ""
+    with open("captchaadress.txt", "r") as capa:
+        if (os.stat("captchaadress.txt").st_size != 0):
+            lineurlindex = capa.readlines()[captchaIndex]
+            urlimg=lineurlindex.split(" /")[0]
+            indexCaptcha = lineurlindex.split("/ ")[1]
+            print(indexCaptcha)
+            eel.captchahar(urlimg)
+    with open("captchaadress.txt", "r") as capa:
+        for line in capa:
+            nbcaptchas = nbcaptchas+1
+        eel.updateCaptchaNb(nbcaptchas)
+with open("captchaadress.txt", 'r+') as cap:
+    cap.seek(0)
+    cap.truncate()
+threading.Thread(target=lambda: every(1, wala)).start()
+def emaily(emailRange):
+    for i in range(emailRange):
+        subprocess.Popen(["python.exe", 'emailt.py'])
+@eel.expose
+def goemail(emailRange):
+    emailRange = int(emailRange)
+    emaily(emailRange)
+if (__name__ == "__main__"):
+    with open ('data\data.vextools', 'r') as data:
+        data = data.read()
+        finished = data.split("= ")[1]
+        finished = finished.split("i")[0]
+        finished = int(finished)
+        ignored = data.split("= ")[2]
+        ignored = ignored.split("t")[0]
+        ignored = int(ignored)
+        total = data.split("= ")[3]
+        total = int(total)
+@eel.expose
+def telgo(country, rangetel):
+    for i in range (int(rangetel)):
+        tel(country)
+@eel.expose
+def tel(country):
+    if country == 'France':
+        randNumber = random.randint(10000000,99999999)
+        prefix = 336
+        phoneNumber = "+"+ str(prefix)+str(randNumber)
+        with open('phone_out.csv', 'a+', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow([phoneNumber])
+    if country == 'United Kingdom':
+        randNumber = random.randint(100000000,999999999)
+        prefix = 447
+        phoneNumber = "+"+ str(prefix)+str(randNumber)
+        with open('phone_out.csv', 'a+', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow([phoneNumber])
+    if country == 'United States':
+        randNumber = random.randint(100000,999999)
+        prefix = 1
+        areacode = random.randint(201,219)
+        phoneNumber = "+"+ str(prefix)+str(areacode)+str(randNumber)
+        with open('phone_out.csv', 'a+', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow([phoneNumber])
+@eel.expose
+def go(rangeGo):
+    global finished
+    global ignored
+    global total
+    total = int(rangeGo) + total
+    for i in range (int(rangeGo)):
+        main()
+    ignored = total - finished
+    eel.output(finished, ignored)
+    with open ('data\data.vextools', 'w') as data:
+        strfinished = "finished = "+str(finished)+ "\n"
+        strignored = "invalid = "+str(ignored)+ "\n"
+        strtotal = "total = "+str(total) + "\n"
+        data.writelines([strfinished,strignored,strtotal])
+@eel.expose
+def add(one,two):
+    print(one,two)
+    result = int(one)+int(two)
+    return result
+@eel.expose
+def minus(one,two):
+    print(one,two)
+    result = int(one)-int(two)
+    return result
+def main():
+    adressesIndex = random.randint(1,17)
+    adressLink = r'Adresses\adresses'+str(adressesIndex)+'.csv'
+    with open(adressLink, 'r') as file:
+        reader = csv.reader(file)
+        count = 0
+        wanted = 10
+        chosen_row = random.choice(list(reader))
+        string = chosen_row[0]
+        adresse1 = string.split(';;')[1]
+        adresse1 = adresse1.split(';')[0]
+        number = string.split(';')[2]
+        number = number.split(';')[0]
+        adresse = number + " " +adresse1
+        cp = string.split(';')[5]
+        ville = string.split(';')[7]
+        country = "France"
+        index = len(adresse)
+        print(index)
+        region = ""
+        if index > 4:
+            with open("regions.txt", 'r+') as fileuiop:
+                for line in fileuiop:
+                    print("oeoeoeooeoe")
+                    lineid = line.split(":")[0]
+                    if(lineid in cp[:2]): region = line.split(":")[1]
+                    print(region)
+            print(adresse, cp, ville, country, region)
+            global finished
+            finished = finished + 1
+            print(finished)
+            with open('adresse_out.csv', 'a+', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow([adresse, cp, ville, country, region])
+            with open('adresse_out.csv', 'r+', newline='') as file:
+                data = file.read()
+                data = data.replace('"', '')
+                lines = data.split("\n")
+                non_empty_lines = [line for line in lines if line.strip() != ""]
+                string_without_empty_lines = ""
+                for line in non_empty_lines:
+                    string_without_empty_lines += line + "\n"
+                file.seek(0)
+                file.truncate(0)
+                file.write(string_without_empty_lines)
+@eel.expose
+def init():
+    eel.output(finished, ignored)
+if (__name__ == "__main__"):
+    eel.start('address.html', port=0, size=(1200,600))
